2395c1a199b7cfda2120adb1929984fe7eec7a22
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-16 22:59:24 -0300

    first commit

---
+++ src/JavaDojo/st/tierone/Hello.java
@@ -0,0 +1,16 @@
+package JavaDojo.st.tierone;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println(
+                "Teste 1..2..3 bip bop"
+        );
+    }
+}
+
+// coment em linha
+
+/*
+wowwowow
+varias linhas feras
+ */
---
+++ src/JavaDojo/st/tierone/primitivos.java
@@ -0,0 +1,21 @@
+package JavaDojo.st.tierone;
+
+public class primitivos {
+    public static void main (String[] args){
+        //psvm
+        // int, double, float, char, byte, short, long, boolean
+
+        int years = 17;
+        double moneyDouble = 1300.654;
+        float moneyFloat = 1250;
+        byte yearByte = 10;
+        short commandsOpen = 15;
+        long commandsClose = 132;
+        boolean rich = false;
+        char gen = 'M';
+
+        System.out.println("Your age: " + years);
+        System.out.println(rich);
+
+    }
+}
