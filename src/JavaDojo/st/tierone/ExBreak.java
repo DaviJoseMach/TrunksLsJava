51535ac325f136638c4725c5496b1775af6fedf5
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-20 19:15:20 -0300

    Add ex break

---
+++ src/JavaDojo/st/tierone/ExBreak.java
@@ -0,0 +1,16 @@
+package JavaDojo.st.tierone;
+
+public class ExBreak {
+    public static void main(String[] args) {
+        double valorCarro = 30000;
+        for(int parcela = 1; parcela <= valorCarro; parcela++ ){
+            double valorParcela = valorCarro / parcela;
+            if(valorParcela >= 1000){
+                System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+            }else{
+                break;
+            }
+
+        }
+    }
+}
