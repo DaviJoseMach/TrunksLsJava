bdce597937d792ad6a602597f18da7bba7325e3b
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-21 20:22:40 -0300

    String[] arrays03Count = {'Esse','Array', 'Teste'}

--- src/JavaDojo/st/tierone/Arrays03.java
+++ src/JavaDojo/st/tierone/Arrays03.java
@@ -2,6 +2,15 @@ package JavaDojo.st.tierone;
 
 public class Arrays03 {
     public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};
+        char[] keys = {'A', 'B', 'C'};
 
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+        for(char i : keys){
+            System.out.println(i);
+        }
     }
 }
