acf5bcc51e50115632ab12de7a2649353bd3b95f
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-21 23:47:52 -0300

    suvbbb

---
+++ src/JavaDojo/st/tierone/ArrayMulti01.java
@@ -0,0 +1,16 @@
+package JavaDojo.st.tierone;
+
+public class ArrayMulti01 {
+    public static void main(String[] args) {
+        int[][] yInfo = new int[3][3];
+        yInfo[0][0] = 27;
+        yInfo[0][1] = 87;
+        yInfo[0][2] = 90;
+
+        System.out.println(yInfo[0][0]);
+
+        for (int i = 0; i < yInfo.length ; i++) {
+            //teste
+        }
+    }
+}
