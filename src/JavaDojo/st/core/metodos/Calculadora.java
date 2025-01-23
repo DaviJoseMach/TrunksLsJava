768a869a11ef7aae08b120130230625a3919b35d
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-23 17:48:53 -0300

    add methodos

--- src/JavaDojo/st/core/metodos/Calculadora.java
+++ src/JavaDojo/st/core/metodos/Calculadora.java
@@ -4,4 +4,13 @@ public class Calculadora {
     public void somaDoisNumbers(){
         System.out.println(10 + 10);
     }
+    public void subDoisNumber(){
+        System.out.println(20 - 5);
+    }
+    public void multiplicador(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+    public double dividir(double num1, double num2){
+        return 0;
+    }
 }
--- src/JavaDojo/st/core/metodos/test/CalcTest.java
+++ src/JavaDojo/st/core/metodos/test/CalcTest.java
@@ -6,6 +6,10 @@ public class CalcTest {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
         calculadora.somaDoisNumbers();
+        calculadora.subDoisNumber();
 
+        int num1 = 15;
+        calculadora.multiplicador(num1, 15);
     }
+
 }
