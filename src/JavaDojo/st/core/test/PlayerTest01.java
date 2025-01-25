b20eed8b8e8ccad5d10f3691315543af353e4077
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-01-25 01:13:58 -0300

    new 2player

--- src/JavaDojo/st/core/test/PlayerTest01.java
+++ src/JavaDojo/st/core/test/PlayerTest01.java
@@ -5,10 +5,13 @@ import JavaDojo.st.core.metodos.Player;
 public class PlayerTest01 {
     public static void main(String[] args) {
         Player player = new Player();
+        Player player2 = new Player();
         player.setNome("Davi");
+        player2.setNome("João");
         player.setIdade(17);
+        player2.setIdade(19);
 
-        System.out.println(player.getNome());
-        System.out.println(player.getIdade());
+        System.out.println(player.getNome() + " " + player.getIdade());
+        System.out.println(player2.getNome() + " " + player2.getIdade());
     }
 }
