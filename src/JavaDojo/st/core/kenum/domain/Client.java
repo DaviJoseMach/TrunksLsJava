13fbd56f0e2f5cb93909e86dd349c907e763da5a
DaviJoseMach <thech.nasa.dj@gmail.com>
2025-02-05 18:10:35 -0300

    enums

--- src/JavaDojo/st/core/kenum/domain/Client.java
+++ src/JavaDojo/st/core/kenum/domain/Client.java
@@ -3,9 +3,22 @@ package JavaDojo.st.core.kenum.domain;
 public class Client {
     private String nome;
     private String tipo;
+    public static final String PESSOA_FISICA = "PESSOA_FISICA";
+    public static final String PESSOA_JURIDICA = "PESSOA_JURIDICA";
 
+    @Override
+    public String
+    toString() {
+        return "Client{" +
+                "nome='" + nome + '\'' +
+                ", tipo='" + tipo + '\'' +
+                '}';
+    }
 
     public Client(String nome, String tipo) {
+        if(!tipo.equals(PESSOA_FISICA) && !tipo.equals(PESSOA_JURIDICA)){
+            return;
+        }
         this.nome = nome;
         this.tipo = tipo;
     }
--- src/JavaDojo/st/core/kenum/test/TestClient.java
+++ src/JavaDojo/st/core/kenum/test/TestClient.java
@@ -1,7 +1,15 @@
 package JavaDojo.st.core.kenum.test;
 
+import JavaDojo.st.core.kenum.domain.Client;
+
 public class TestClient {
     public static void main(String[] args) {
+        Client client = new Client("Levi", "PESSOA_FISICA");
+        Client client2 = new Client("Levi", "PESSOA_JURIDICA");
+        Client client3 = new Client("Levi", "pesso ajurai");
 
+        System.out.println(client);
+        System.out.println(client2);
+        System.out.println(client3);
     }
 }
